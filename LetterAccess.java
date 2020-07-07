e20c61aab4d343e7711bab8216643f3e2ccd8e86
Elisco360 <64059839+Elisco360@users.noreply.github.com>
2020-07-07 12:26:00 +0000

    Update LetterAccess.java

--- LetterAccess.java
+++ LetterAccess.java
@@ -1,51 +1,50 @@
-package Assignment;
-
-import java.util.Arrays;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-class content {
-    private String fileName;
-    private int[] letterTimes;
+class Content {
+    private int[] frequencies = new int[26];;
+    private String fileContent;
 
-    public content(String fileName){
-        this.fileName = fileName;
-        this.letterTimes = new int[26];
-    }
-    
-    public void retrieveFile() throws FileNotFoundException {
-        String fileContent = "";
-        File file = new File("/Users/elibo/OneDrive/Desktop/" + fileName);
+
+    /**
+     * Constructor that reads a file by means of taking filePath and scans its content into fileContent.
+     * It also calls on the letterFrequency() method to automatically obtain the frequencies without the user calling it.
+     * @param filePath this parameter directs the File class to access the text file for reading.
+     * @throws FileNotFoundException this exception is thrown to cater for wrong file paths and names.
+     */
+    public Content(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
         Scanner scanFile = new Scanner(file);
 
-        while (scanFile.hasNextLine()) {
-            fileContent = fileContent.concat(scanFile.nextLine());
+        fileContent = "";
+        while(scanFile.hasNextLine()){
+            this.fileContent = this.fileContent.concat(scanFile.nextLine().toLowerCase());
         }
-        this.fileName = fileContent.toLowerCase();
-        System.out.println(fileName);
+
+        letterFrequency();
     }
 
-    public String charNumber() {
-        String alphabets =  "abcdefghijklmnopqrstuvwxyz";
-        char[] myAlphabets =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        for(int index = 0; index != fileName.length(); index++){
-            for(char eachChar: myAlphabets){
-                if(eachChar == fileName.charAt(index)){
-                    letterTimes[alphabets.indexOf(eachChar)]++;
+
+    /**
+     * This method stores the frequencies of each letter by iterating through fileContent and myAlphabets simultaneously
+     */
+    public void letterFrequency() {
+        char[] myAlphabets = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        for (int index = 0; index != fileContent.length(); index++) {
+            for (char eachChar : myAlphabets) {
+                if (eachChar == fileContent.charAt(index)) {
+                    frequencies[new String(myAlphabets).indexOf(eachChar)]++;
                 }
             }
         }
-        return Arrays.toString(letterTimes);
-    }
-}
-
-public class DemoCode {
-    public static void main(String[] args) throws FileNotFoundException{
-        content myContent = new content("myself.txt");
-        myContent.retrieveFile();
-        System.out.println(myContent.charNumber()
-        );
     }
 
-}
+
+    /**
+     * This method is responsible for making our frequencies referencable in other files
+     * @return it returns the int array of frequencies of each letter in order of alphabets
+     */
+    public int[] getLetterFrequency() { return frequencies; }
